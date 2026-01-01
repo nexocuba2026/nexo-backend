34f4c321eb0f801cc9e2d3644121bfe1fd4f88e6
nexo-backend <nexocuba2026@gmail.com>
2026-01-01 15:40:18 -0500

    Update category.py

--- app/models/category.py
+++ app/models/category.py
@@ -1,1 +1,8 @@
-# Modelo de categoría
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True)
