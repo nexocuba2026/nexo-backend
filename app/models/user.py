c85e15eff593f6f7cc9153fa00771118498a2a95
nexo-backend <nexocuba2026@gmail.com>
2026-01-01 15:39:17 -0500

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,1 +1,9 @@
-# Modelo de usuario
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    phone = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
