5296cfe46b1bb75ec9f6963f41ffacfcf8d26331
nexo-backend <nexocuba2026@gmail.com>
2026-01-01 15:38:38 -0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,16 @@
-# archivo de base de datos
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///./nexo.db"
+
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
