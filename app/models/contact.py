0d75ccf34a873496737fa46241c608cf68ac4785
nexo-backend <nexocuba2026@gmail.com>
2026-01-01 15:40:44 -0500

    Update contact.py

--- app/models/contact.py
+++ app/models/contact.py
@@ -1,1 +1,10 @@
-# Modelo de contacto
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+class Contact(Base):
+    __tablename__ = "contacts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_phone = Column(String)
+    service_name = Column(String)
+    contact_type = Column(String)
