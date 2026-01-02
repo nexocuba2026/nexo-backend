cf3a61a68391c5213a26bab21d19798dc5412e90
nexo-backend <nexocuba2026@gmail.com>
2026-01-01 20:03:45 -0500

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -3,7 +3,11 @@ from app.database import Base
 
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
-    phone = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
+    username = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+
+class Category(Base):
+    __tablename__ = "categories"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
