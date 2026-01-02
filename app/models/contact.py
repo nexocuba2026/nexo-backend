2aba09a105b1ff591788544b31dcec582596a8ee
nexo-backend <nexocuba2026@gmail.com>
2026-01-01 20:03:04 -0500

    Update contact.py

--- app/models/contact.py
+++ app/models/contact.py
@@ -3,8 +3,7 @@ from app.database import Base
 
 class Contact(Base):
     __tablename__ = "contacts"
-
     id = Column(Integer, primary_key=True, index=True)
-    user_phone = Column(String)
-    service_name = Column(String)
+    user_phone = Column(String, nullable=False)
+    service_name = Column(String, nullable=False)
     contact_type = Column(String)
