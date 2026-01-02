32a5fdacfe51238f1331c89584bdcc857369718f
nexo-backend <nexocuba2026@gmail.com>
2026-01-01 20:04:23 -0500

    Update category.py

--- app/models/category.py
+++ app/models/category.py
@@ -1,8 +1,13 @@
 from sqlalchemy import Column, Integer, String
 from app.database import Base
 
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+
 class Category(Base):
     __tablename__ = "categories"
-
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True)
+    name = Column(String, nullable=False)
